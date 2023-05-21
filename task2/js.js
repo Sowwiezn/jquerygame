254b6bb4a0bf6ce734b4cf794b8a71ac796d7ffd
Sowwiezn <115182964+Sowwiezn@users.noreply.github.com>
2023-05-21 22:27:39 +0300

    Update js.js

--- task2/js.js
+++ task2/js.js
@@ -4,7 +4,7 @@ let time;
     if (timeStorage.getItem("timer") != null) {
         time = parseInt(timeStorage.getItem("timer"));
     } else {
-        time = 20;
+        time = 60;
         timeStorage.setItem("timer", time);
     }
 
